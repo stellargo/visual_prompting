6b78b47b160ac7e18ba96272ff4ecb741a22ac2f
AakankshaSanctis <sanctisaakanksha@gmail.com>
2022-12-18 01:30:49 -0500

    comment out train

--- main_clip.py
+++ main_clip.py
@@ -156,13 +156,13 @@ def main():
                                 transform= preprocess)
 
     print(args.batch_size)
-    train_loader = DataLoader(train_dataset,
-                              batch_size=args.batch_size, pin_memory=False,
-                              num_workers=args.num_workers, shuffle=True)
+    # train_loader = DataLoader(train_dataset,
+    #                           batch_size=args.batch_size, pin_memory=False,
+    #                           num_workers=args.num_workers, shuffle=True)
 
-    val_loader = DataLoader(val_dataset,
-                            batch_size=args.batch_size, pin_memory=False,
-                            num_workers=args.num_workers, shuffle=False)
+    # val_loader = DataLoader(val_dataset,
+    #                         batch_size=args.batch_size, pin_memory=False,
+    #                         num_workers=args.num_workers, shuffle=False)
 
     class_names = train_dataset.classes
     class_names = refine_classname(class_names)
@@ -197,42 +197,42 @@ def main():
         wandb.run.name = args.filename
         wandb.watch(prompter, criterion, log='all', log_freq=10)
 
-    if args.evaluate:
-        acc1 = validate(val_loader, texts, model, prompter, criterion, args)
-        return
+    # if args.evaluate:
+    #     acc1 = validate(val_loader, texts, model, prompter, criterion, args)
+    #     return
 
     epochs_since_improvement = 0
 
     for epoch in range(args.epochs):
-
+        pass
         # train for one epoch
-        train(train_loader, texts, model, prompter, optimizer, scheduler, criterion, scaler, epoch, args)
+        # train(train_loader, texts, model, prompter, optimizer, scheduler, criterion, scaler, epoch, args)
 
-        # evaluate on validation set
-        acc1 = validate(val_loader, texts, model, prompter, criterion, args)
+        # # evaluate on validation set
+        # acc1 = validate(val_loader, texts, model, prompter, criterion, args)
 
-        # remember best acc@1 and save checkpoint
-        is_best = acc1 > best_acc1
-        best_acc1 = max(acc1, best_acc1)
+        # # remember best acc@1 and save checkpoint
+        # is_best = acc1 > best_acc1
+        # best_acc1 = max(acc1, best_acc1)
 
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'state_dict': prompter.state_dict(),
-            'best_acc1': best_acc1,
-            'optimizer': optimizer.state_dict(),
-        }, args, is_best=is_best)
+        # save_checkpoint({
+        #     'epoch': epoch + 1,
+        #     'state_dict': prompter.state_dict(),
+        #     'best_acc1': best_acc1,
+        #     'optimizer': optimizer.state_dict(),
+        # }, args, is_best=is_best)
 
-        if is_best:
-            epochs_since_improvement = 0
-        else:
-            epochs_since_improvement += 1
-            print(f"There's no improvement for {epochs_since_improvement} epochs.")
+        # if is_best:
+        #     epochs_since_improvement = 0
+        # else:
+        #     epochs_since_improvement += 1
+        #     print(f"There's no improvement for {epochs_since_improvement} epochs.")
 
-            if epochs_since_improvement >= args.patience:
-                print("The training halted by early stopping criterion.")
-                break
+        #     if epochs_since_improvement >= args.patience:
+        #         print("The training halted by early stopping criterion.")
+        #         break
 
-    wandb.run.finish()
+    # wandb.run.finish()
 
 
 def train(train_loader, texts, model, prompter, optimizer, scheduler, criterion, scaler, epoch, args):

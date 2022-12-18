b23b3ab9b0621c7c566715433a99babc8503b7da
AakankshaSanctis <sanctisaakanksha@gmail.com>
2022-12-17 19:47:39 -0500

    added cifar resnet model download

--- models/download_models.sh
+++ models/download_models.sh
@@ -5,3 +5,4 @@ pip install git+https://github.com/openai/CLIP.git
 git clone https://github.com/google-research/big_transfer.git
 pip install -r big_transfer/bit_pytorch/requirements.txt
 wget https://storage.googleapis.com/bit_models/BiT-M-R50x1.npz
+wget https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet56-f2eff4c8.pt

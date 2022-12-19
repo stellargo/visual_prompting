5295e17694922a927cc908d674099476e3e2c9fa
Sumit Sarin <sumitsarinofficial@gmail.com>
2022-12-18 19:39:21 -0500

    abc

--- cifar_c.py
+++ cifar_c.py
@@ -219,7 +219,7 @@ def main():
     epochs_since_improvement = 0
 
     preprocess = transforms.Compose([
-        transforms.Resize(size=224, interpolation="bicubic", max_size=None, antialias=None),
+        transforms.Resize(size=224, interpolation=transforms.InterpolationMode.BICUBIC, max_size=None, antialias=None),
         transforms.CenterCrop(size=(224, 224)),
         transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
     ])
